60b76919ffde18d6a97fd5e4bf0c2e8cd55f931e
Rocky <33016454+rockyr8@users.noreply.github.com>
2018-01-29 14:31:16 +0800

    kimmidoll

--- database/mysql.go
+++ database/mysql.go
@@ -11,7 +11,7 @@ var SqlDB *sql.DB
 
 func init() {
 	var err error
-	SqlDB, err = sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/fishtimer?parseTime=true")
+	SqlDB, err = sql.Open("mysql", "rocky:3515666888_wcwzecePWX@tcp(45.77.43.2:3306)/kimmidoll?parseTime=true")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
